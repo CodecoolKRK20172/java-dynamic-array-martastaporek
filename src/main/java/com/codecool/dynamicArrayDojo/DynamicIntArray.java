d228369a6378f1f233f23a927de215fde5b7fdb2
martastaporek <staporekmarta1@gmail.com>
2018-04-24 12:40:38 +0200

    Create signatures reguired by tests and make method add pass

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -3,4 +3,50 @@ package com.codecool.dynamicArrayDojo;
 // put your code here!
 public class DynamicIntArray {
 
+    private int [] array;
+
+    public DynamicIntArray() {
+
+        this.array = new int[0];
+    }
+
+    public DynamicIntArray(int size) {
+
+        this.array = new int [size];
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+
+    public void add(int number) {
+
+
+        int [] newArray = new int[this.array.length + 1];
+
+        for(int i=0; i < this.array.length; i++){
+            newArray[i] = this.array[i];
+        }
+        newArray [this.array.length] = number;
+        setArray(newArray);
+
+    }
+
+    public void remove(int number){
+
+    }
+
+    public void insert(int position, int number){
+
+    }
+
+    @Override
+    public String toString() {
+        String toString = "";
+        for(int elem : this.array){
+            toString += " " + elem;
+
+        }
+        return toString;
+    }
 }
